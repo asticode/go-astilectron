d6e408585fff1b7dc4ef3cdaf02cbf06c031bbd9
Quentin Renard <contact@asticode.com>
2017-05-07 11:37:27 +0200

    Fixed tests

--- display_test.go
+++ display_test.go
@@ -11,7 +11,7 @@ func TestDisplay(t *testing.T) {
 	var o = &DisplayOptions{
 		Bounds:       &RectangleOptions{PositionOptions: PositionOptions{X: PtrInt(1), Y: PtrInt(2)}, SizeOptions: SizeOptions{Height: PtrInt(3), Width: PtrInt(4)}},
 		Rotation:     PtrInt(5),
-		ScaleFactor:  PtrInt(6),
+		ScaleFactor:  PtrFloat(6),
 		Size:         &SizeOptions{Height: PtrInt(7), Width: PtrInt(8)},
 		TouchSupport: PtrStr("available"),
 		WorkArea:     &RectangleOptions{PositionOptions: PositionOptions{X: PtrInt(9), Y: PtrInt(10)}, SizeOptions: SizeOptions{Height: PtrInt(11), Width: PtrInt(12)}},
@@ -21,7 +21,7 @@ func TestDisplay(t *testing.T) {
 	assert.Equal(t, Rectangle{Position: Position{X: 1, Y: 2}, Size: Size{Height: 3, Width: 4}}, d.Bounds())
 	assert.True(t, d.IsPrimary())
 	assert.Equal(t, 5, d.Rotation())
-	assert.Equal(t, 6, d.ScaleFactor())
+	assert.Equal(t, float64(6), d.ScaleFactor())
 	assert.Equal(t, Size{Height: 7, Width: 8}, d.Size())
 	assert.True(t, d.IsTouchAvailable())
 	assert.Equal(t, Rectangle{Position: Position{X: 9, Y: 10}, Size: Size{Height: 11, Width: 12}}, d.WorkArea())
