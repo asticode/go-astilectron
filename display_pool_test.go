116b8a35e82dfe8d05946ebe21e6b7b8b5439258
Quentin Renard <qrenard@molotov.tv>
2017-05-24 16:24:11 +0200

    ID in DisplayOptions is now an int64

--- display_pool_test.go
+++ display_pool_test.go
@@ -13,33 +13,33 @@ func TestDisplayPool(t *testing.T) {
 	// Test update
 	dp.update(&EventDisplays{
 		All: []*DisplayOptions{
-			{ID: PtrInt(1), Rotation: PtrInt(1)},
-			{ID: PtrInt(2)},
+			{ID: PtrInt64(1), Rotation: PtrInt(1)},
+			{ID: PtrInt64(2)},
 		},
-		Primary: &DisplayOptions{ID: PtrInt(2)},
+		Primary: &DisplayOptions{ID: PtrInt64(2)},
 	})
 	assert.Len(t, dp.all(), 2)
-	assert.Equal(t, 2, *dp.primary().o.ID)
+	assert.Equal(t, int64(2), *dp.primary().o.ID)
 
 	// Test removing one display
 	dp.update(&EventDisplays{
 		All: []*DisplayOptions{
-			{ID: PtrInt(1), Rotation: PtrInt(2)},
+			{ID: PtrInt64(1), Rotation: PtrInt(2)},
 		},
-		Primary: &DisplayOptions{ID: PtrInt(1)},
+		Primary: &DisplayOptions{ID: PtrInt64(1)},
 	})
 	assert.Len(t, dp.all(), 1)
 	assert.Equal(t, 2, dp.all()[0].Rotation())
-	assert.Equal(t, 1, *dp.primary().o.ID)
+	assert.Equal(t, int64(1), *dp.primary().o.ID)
 
 	// Test adding a new one
 	dp.update(&EventDisplays{
 		All: []*DisplayOptions{
-			{ID: PtrInt(1)},
-			{ID: PtrInt(3)},
+			{ID: PtrInt64(1)},
+			{ID: PtrInt64(3)},
 		},
-		Primary: &DisplayOptions{ID: PtrInt(1)},
+		Primary: &DisplayOptions{ID: PtrInt64(1)},
 	})
 	assert.Len(t, dp.all(), 2)
-	assert.Equal(t, 1, *dp.primary().o.ID)
+	assert.Equal(t, int64(1), *dp.primary().o.ID)
 }
