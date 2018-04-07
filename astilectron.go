74dd4e179ce65ee50476cd72df017e7a4eba69ca
john dev <joh.dev@gmail.com>
2018-04-07 10:37:41 +0200

    updated to v0.20.0

--- astilectron.go
+++ astilectron.go
@@ -19,7 +19,7 @@ import (
 // Versions
 const (
 	DefaultAcceptTCPTimeout = 30 * time.Second
-	VersionAstilectron      = "0.19.0"
+	VersionAstilectron      = "0.20.0"
 	VersionElectron         = "1.8.1"
 )
 
