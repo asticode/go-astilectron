2c482a46fc9daaff682a9ddaab0dc11e96532ff2
sereeena <serenalien@gmail.com>
2020-04-17 01:46:33 +0000

    Closes #229

--- session.go
+++ session.go
@@ -6,9 +6,11 @@ import (
 
 // Session event names
 const (
-	EventNameSessionCmdClearCache     = "session.cmd.clear.cache"
-	EventNameSessionEventClearedCache = "session.event.cleared.cache"
-	EventNameSessionEventWillDownload = "session.event.will.download"
+	EventNameSessionCmdClearCache       = "session.cmd.clear.cache"
+	EventNameSessionEventClearedCache   = "session.event.cleared.cache"
+	EventNameSessionEventWillDownload   = "session.event.will.download"
+	EventNameSessionCmdFlushStorage     = "session.cmd.flush.storage"
+	EventNameSessionEventFlushedStorage = "session.event.flushed.storage"
 )
 
 // Session represents a session
@@ -30,5 +32,14 @@ func (s *Session) ClearCache() (err error) {
 		return
 	}
 	_, err = synchronousEvent(s.ctx, s, s.w, Event{Name: EventNameSessionCmdClearCache, TargetID: s.id}, EventNameSessionEventClearedCache)
+	return
+}
+
+// FlushStorage writes any unwritten DOMStorage data to disk
+func (s *Session) FlushStorage() (err error) {
+	if err = s.ctx.Err(); err != nil {
+		return
+	}
+	_, err = synchronousEvent(s.ctx, s, s.w, Event{Name: EventNameSessionCmdFlushStorage, TargetID: s.id}, EventNameSessionEventFlushedStorage)
 	return
 }
