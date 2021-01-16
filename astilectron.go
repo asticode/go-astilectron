6aa7218e3824c7f63e15995e628c95a8b05afcd2
Quentin Renard <contact@asticode.com>
2021-01-16 12:21:35 +0100

    Bumped astilectron

--- astilectron.go
+++ astilectron.go
@@ -13,7 +13,7 @@ import (
 // Versions
 const (
 	DefaultAcceptTCPTimeout   = 30 * time.Second
-	DefaultVersionAstilectron = "0.43.1"
+	DefaultVersionAstilectron = "0.44.0"
 	DefaultVersionElectron    = "11.1.0"
 )
 
