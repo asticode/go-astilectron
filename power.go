93005a8f8f05f8924e3cc0a66cdc01f697fa76bd
Maddie Zhan <maddie.zhan@cynovan.com>
2022-05-09 10:26:56 +0800

    Change powerMonitor events constants names

--- power.go
+++ power.go
@@ -2,13 +2,13 @@ package astilectron
 
 // Power event names
 const (
-	PowerEventSuspend             = "power.event.suspend"
-	PowerEventResume              = "power.event.resume"
-	PowerEventOnAC                = "power.event.on.ac"
-	PowerEventOnBattery           = "power.event.on.battery"
-	PowerEventShutdown            = "power.event.shutdown"
-	PowerEventLockScreen          = "power.event.lock.screen"
-	PowerEventUnlockScreen        = "power.event.unlock.screen"
-	PowerEventUserDidBecomeActive = "power.event.user.did.become.active"
-	PowerEventUserDidResignActive = "power.event.user.did.resign.active"
+	EventNamePowerSuspend             = "power.event.suspend"
+	EventNamePowerResume              = "power.event.resume"
+	EventNamePowerOnAC                = "power.event.on.ac"
+	EventNamePowerOnBattery           = "power.event.on.battery"
+	EventNamePowerShutdown            = "power.event.shutdown"
+	EventNamePowerLockScreen          = "power.event.lock.screen"
+	EventNamePowerUnlockScreen        = "power.event.unlock.screen"
+	EventNamePowerUserDidBecomeActive = "power.event.user.did.become.active"
+	EventNamePowerUserDidResignActive = "power.event.user.did.resign.active"
 )
