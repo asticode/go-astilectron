eace8e54ebb08de016b835406c2871a71c89ec19
maddie <maddie@users.noreply.github.com>
2022-05-09 14:50:56 +0800

    Add powerMonitor events (#364)
    * Add power events
    
    * Bump astilectron version to 0.54.0
    
    * Remove `power` target
    
    * Change powerMonitor events constants names

---
+++ power.go
@@ -0,0 +1,14 @@
+package astilectron
+
+// Power event names
+const (
+	EventNamePowerSuspend             = "power.event.suspend"
+	EventNamePowerResume              = "power.event.resume"
+	EventNamePowerOnAC                = "power.event.on.ac"
+	EventNamePowerOnBattery           = "power.event.on.battery"
+	EventNamePowerShutdown            = "power.event.shutdown"
+	EventNamePowerLockScreen          = "power.event.lock.screen"
+	EventNamePowerUnlockScreen        = "power.event.unlock.screen"
+	EventNamePowerUserDidBecomeActive = "power.event.user.did.become.active"
+	EventNamePowerUserDidResignActive = "power.event.user.did.resign.active"
+)
